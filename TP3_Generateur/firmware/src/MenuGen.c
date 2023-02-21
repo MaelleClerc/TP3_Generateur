87593e4ecf1d172af7e30dee8e435ecbc072699f
ETMLNET\maeclerc <a@a.ch>
2023-02-21 15:00:51 +0100

    Début du menu

--- TP3_Generateur/firmware/src/MenuGen.c
+++ TP3_Generateur/firmware/src/MenuGen.c
@@ -9,6 +9,9 @@
 #include <stdint.h>                   
 #include <stdbool.h>
 #include "MenuGen.h"
+#include "GesPec12.h"
+
+MENU_STATES MENU_DATA;
 
 
 // Initialisation du menu et des paramètres
@@ -20,6 +23,119 @@ void MENU_Initialize(S_ParamGen *pParam)
 // Execution du menu, appel cyclique depuis l'application
 void MENU_Execute(S_ParamGen *pParam)
 {
+    // Variable locales
+    static uint16_t Frequence_Selection = 20;
+    
+    switch (MENU_DATA)
+    {
+        case MENU_STATE_FORME:
+            
+            // Gestion de la sortie de l'etat
+            if (Pec12.Inc == 1)
+            {
+                MENU_DATA = MENU_STATE_FREQUENCE;
+            }
+            else if (Pec12.Dec == 1)
+            {
+                MENU_DATA = MENU_STATE_OFFSET;
+            }
+            else if (Pec12.OK == 1)
+            {
+                MENU_DATA = MENU_STATE_FORME_VALUE;
+            }
+            
+            break;
+            
+        case MENU_STATE_FORME_VALUE:
+            
+            break;
+            
+        case MENU_STATE_FREQUENCE:
+            
+            // Gestion de la sortie de l'etat
+            if (Pec12.Inc == 1)
+            {
+                MENU_DATA = MENU_STATE_AMPLITUDE;
+            }
+            else if (Pec12.Dec == 1)
+            {
+                MENU_DATA = MENU_STATE_FORME;
+            }
+            else if (Pec12.OK == 1)
+            {
+                MENU_DATA = MENU_STATE_FREQUENCE_VALUE;
+            }
+            
+            break;
+            
+        case MENU_STATE_FREQUENCE_VALUE:
+            
+            if (Pec12.Inc == 1)
+            {
+                Frequence_Selection += 20;
+                
+                if (Frequence_Selection == 2020)
+                {
+                    Frequence_Selection = 20;
+                }
+            }
+            else if (Pec12.Dec == 1)
+            {
+                if (Frequence_Selection == 20)
+                {
+                    Frequence_Selection = 2020;
+                }
+                
+                Frequence_Selection -= 20;
+            }
+            
+            break;
+            
+        case MENU_STATE_AMPLITUDE:
+            
+            // Gestion de la sortie de l'etat
+            if (Pec12.Inc == 1)
+            {
+                MENU_DATA = MENU_STATE_OFFSET;
+            }
+            else if (Pec12.Dec == 1)
+            {
+                MENU_DATA = MENU_STATE_FREQUENCE;
+            }
+            else if (Pec12.OK == 1)
+            {
+                MENU_DATA = MENU_STATE_AMPLITUDE_VALUE;
+            }
+            
+            break;
+            
+        case MENU_STATE_AMPLITUDE_VALUE:
+            
+            break;
+            
+        case MENU_STATE_OFFSET:
+            
+            // Gestion de la sortie de l'etat
+            if (Pec12.Inc == 1)
+            {
+                MENU_DATA = MENU_STATE_FORME;
+            }
+            else if (Pec12.Dec == 1)
+            {
+                MENU_DATA = MENU_STATE_AMPLITUDE;
+            }
+            else if (Pec12.OK == 1)
+            {
+                MENU_DATA = MENU_STATE_OFFSET_VALUE;
+            }
+            
+            break;
+            
+        case MENU_STATE_OFFSET_VALUE:
+            
+            break;
+            
+    }
 }
 
 
--- TP3_Generateur/firmware/src/MenuGen.h
+++ TP3_Generateur/firmware/src/MenuGen.h
@@ -12,6 +12,31 @@
 #include <stdint.h>
 #include "DefMenuGen.h"
 
+// *****************************************************************************
+/* Application states
+
+  Summary:
+    Menu states enumeration
+
+  Description:
+    This enumeration defines the valid menu states.  These states
+    determine the behavior of the application at various times.
+*/
+
+typedef enum
+{
+	/* Menu's state machine's initial state. */
+	MENU_STATE_FORME=0,
+    MENU_STATE_FORME_VALUE=1,
+    MENU_STATE_FREQUENCE=2,
+    MENU_STATE_FREQUENCE_VALUE=3,
+    MENU_STATE_AMPLITUDE=4,
+    MENU_STATE_AMPLITUDE_VALUE=5,
+    MENU_STATE_OFFSET=6,
+    MENU_STATE_OFFSET_VALUE=7
+	
+} MENU_STATES;
+
 
 void MENU_Initialize(S_ParamGen *pParam);
 
